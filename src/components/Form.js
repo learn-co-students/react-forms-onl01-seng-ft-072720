d8052c87f09285bebc77fb0acb4709d9420739ac
NickSpangler <Spangleyman@mac.com>
2020-11-26 11:02:41 -0500

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,13 @@ import React from 'react';
 class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
+    lastName: "Henry",
+    submittedData: [],
+    box: false
   }
 
   handleFirstNameChange = event => {
+    console.log(event.target.value)
     this.setState({
       firstName: event.target.value
     })
@@ -18,12 +21,41 @@ class Form extends React.Component {
     })
   }
 
+  handleSubmit= (e) => {
+    e.preventDefault();
+    let formData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName
+    }
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray})
+  }
+
+  listOfSubmissions = () => {
+    return this.state.submittedData.map((data, index) => {
+      return <div><span key={`first-name-${index}`}>{data.firstName}</span> <span key={`last-name-${index}`}>{data.lastName}</span></div>
+    })
+  }
+
+  handleBoxCheck = (e) => {
+    console.log('Before Change State ' + this.state.box)
+    this.setState({
+      box: e.target.checked
+    }, () => console.log('After Change State ' + this.state.box))
+    
+  }
+
   render() {
     return (
-      <form>
+      <div>
+      <form onSubmit={e => this.handleSubmit(e)}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+        <input type='checkbox' name="box" onChange={event => this.handleBoxCheck(event)} checked={this.state.box}></input>
+        <button type='submit'>Submit</button>
       </form>
+      {this.listOfSubmissions()}
+      </div>
     )
   }
 }
